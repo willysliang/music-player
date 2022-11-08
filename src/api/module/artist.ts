40a5aae391bd0c486b3aad399d19dc5a6a5b3e1a
liangwl <liangwl@sunhotin.com>
2022-11-08 17:59:27 +0800

    feat(views -> artist -> ArtistAlbum): 歌手专辑模块组件实现

--- src/api/module/artist.ts
+++ src/api/module/artist.ts
@@ -2,12 +2,13 @@
  * @ Author: willysliang
  * @ Create Time: 2022-10-11 09:35:48
  * @ Modified by: willysliang
- * @ Modified time: 2022-11-07 10:12:27
+ * @ Modified time: 2022-11-08 17:13:30
  * @ Description: 歌手详情的接口
  */
 import type { ArtistDetail } from '@/types/artist'
-import { Mv } from '@/types/mv'
-import { Song } from '@/types/song'
+import type { Mv } from '@/types/mv'
+import type { Song } from '@/types/song'
+import type { Album } from '@/types/album'
 import http from '@/utils/http'
 
 /***
@@ -48,6 +49,14 @@ export const useArtistSongs = async (
 export const useArtistMv = async (id: number, limit = 10, offset = 0) => {
   return await http.get<{ mvs: Mv[] }>({
     url: '/api/artist/mv',
+    params: { id, limit, offset }
+  })
+}
+
+/** 获取歌手专辑 */
+export const useArtistAlbum = async (id: number, limit = 10, offset = 0) => {
+  return await http.get<{ hotAlbums: Album[] }>({
+    url: '/api/artist/album',
     params: { id, limit, offset }
   })
 }
