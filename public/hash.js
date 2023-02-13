9d877fcc005f9ee1f7006ee5095aed867ea476a5
liangwl <liangwl@sunhotin.com>
2023-02-13 15:51:44 +0800

    feat(views -> demo -> FileUpload): FileChunkUpload 文件切块上传

--- public/hash.js
+++ public/hash.js
@@ -1,7 +1,15 @@
-// 导入脚本
+/**
+ * @ Author: willysliang
+ * @ Create Time: 2023-02-10 17:58:58
+ * @ Modified by: willysliang
+ * @ Modified time: 2023-02-13 15:51:28
+ * @ Description: FileChunkUpload 文件切块上传
+ */
+
+/** 导入脚本 */
 self.importScripts('/spark-md5.min.js')
 
-// 生成文件 hash
+/** 生成文件 hash */
 self.onmessage = (e) => {
   const { fileChunkList } = e.data
   const spark = new self.SparkMD5.ArrayBuffer()
