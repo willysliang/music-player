e182425fee6a0c4dfdb1bbaf7a190cfd5afcbf4b
liangwl <liangwl@sunhotin.com>
2023-03-08 13:33:38 +0800

    feat(demo -> virtualList): 虚拟列表

--- src/views/demo/test/index.vue
+++ src/views/demo/test/index.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable n/no-callback-literal -->
 <!-- eslint-disable no-unmodified-loop-condition -->
 <!-- eslint-disable @typescript-eslint/no-empty-function -->
 <!-- eslint-disable brace-style -->
@@ -13,20 +14,80 @@
 <script setup lang="ts">
 import { onMounted, ref, reactive, computed, watch } from 'vue'
 import { cloneDeep } from 'lodash'
-import VirtualList from '../virtualList/VirtualList.vue'
+// import VirtualList from './VirtualList.vue'
+import VirtualList from './virtual-list.vue'
 
 const add = () => {
   console.log('add')
 }
 
-const list = ref(
-  new Array(268435)
-    .fill({ name: 'willy', id: 0 })
-    .map((item, i) => ({ name: `${item.name}${i}`, id: i })),
-)
+const data = ref<Array<any>>([])
+
+for (let i = 1; i <= 1000000; i++) {
+  data.value.push({
+    id: i,
+    value: '字符内容'.repeat(Math.random() * 100),
+  })
+}
+
+const list: any[] = Array.from({ length: 10000 })
+  .fill({ text: '元素' })
+  .map((item: any) => ({ ...item, height: 200 * (Math.random() * 0.5 + 0.5) }))
 </script>
 
 <template>
-  <!-- <el-button @click="add">delete</el-button> -->
-  <VirtualList :listdata="list" :item-size="100" />
+    <div class="fill">
+    <div class="list-wrapper">
+      <virtual-list :list="list">
+        <template
+          v-for="(item, index) in list"
+          :key="index"
+          #[`item${index}`]
+        >
+          <div class="real-list-item" :style="{ height: `${item.height}px` }">
+            <span>{{ item.text }}{{ index }}</span>
+          </div>
+        </template>
+        <template #skeleton>
+          <div class="skeleton">骨架</div>
+        </template>
+      </virtual-list>
+    </div>
+  </div>
+
+ <!--  <VirtualList :list-data="data">
+    <template #default="{ listItem }">
+      <div> <span class="text-red-600">{{ listItem.idx }}</span> {{ listItem.value }}</div>
+    </template>
+  </VirtualList> -->
 </template>
+
+<style lang="scss">
+
+.list-wrapper {
+	width: 100%;
+	height: 500px;
+}
+
+.real-list-item {
+	border: 1px solid #333;
+	box-sizing: border-box;
+	margin-top: 16px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.skeleton {
+	margin-top: 16px;
+	background-color: #ccc;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
